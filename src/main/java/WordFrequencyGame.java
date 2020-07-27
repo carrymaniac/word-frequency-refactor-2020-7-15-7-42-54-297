ae9890633260ba2d8068776f74499de23a76a19a
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-27 15:56:02 +0800

    [henry] refactor: extract generateWordFrequencyList method and rename generateWordInfoFrequency to generateWordInfoFrequencyString

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,57 +9,33 @@ public class WordFrequencyGame {
         } else {
 
             try {
-
-                //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
-
-                List<WordInfo> wordInfoList = new ArrayList<>();
-                for (String s : arr) {
-                    WordInfo wordInfo = new WordInfo(s, 1);
-                    wordInfoList.add(wordInfo);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordInfo>> map =getListMap(wordInfoList);
-
-                List<WordInfo> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()) {
-                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(wordInfo);
-                }
-                wordInfoList = list;
+                List<WordInfo> wordInfoList = generateWordFrequencyList(inputStr);
 
                 wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
-                return generateWordInfoFrequency(wordInfoList);
+                return generateWordInfoFrequencyString(wordInfoList);
             } catch (Exception e) {
                 return "Calculate Error";
             }
         }
     }
 
-    private String generateWordInfoFrequency(List<WordInfo> wordInfoList) {
+    private List<WordInfo> generateWordFrequencyList(String sentence){
+        List<String> strings = Arrays.asList(sentence.split("\\s+"));
+        List<WordInfo> list = new ArrayList<>();
+        for(String uniqueWord:new HashSet<>(strings)){
+            long count = strings.stream().filter(word -> word.equals(uniqueWord)).count();
+            WordInfo wordInfo = new WordInfo(uniqueWord,(int)count);
+            list.add(wordInfo);
+        }
+        return list;
+    }
+    private String generateWordInfoFrequencyString(List<WordInfo> wordInfoList) {
         StringJoiner joiner = new StringJoiner("\n");
         for (WordInfo w : wordInfoList) {
             String s = w.getValue() + " " +w.getWordCount();
             joiner.add(s);
         }
         return joiner.toString();
-    }
-
-    private Map<String, List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(wordInfo.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getValue(), arr);
-            }
-            else {
-                map.get(wordInfo.getValue()).add(wordInfo);
-            }
-        }
-        return map;
     }
 }
