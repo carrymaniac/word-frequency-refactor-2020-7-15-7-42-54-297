2ff02ce2a30302ce4222ba2c4c8bb79a1b9b0356
OOCLDM\HUANGHE7 <henry.huang@oocl.com>
2020-07-27 15:57:00 +0800

    [henry] refactor: format the code

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -2,17 +2,12 @@ import java.util.*;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
-
-
-        if (inputStr.split("\\s+").length==1) {
+        if (inputStr.split("\\s+").length == 1) {
             return inputStr + " 1";
         } else {
-
             try {
                 List<WordInfo> wordInfoList = generateWordFrequencyList(inputStr);
-
                 wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
                 return generateWordInfoFrequencyString(wordInfoList);
             } catch (Exception e) {
                 return "Calculate Error";
@@ -20,20 +15,21 @@ public class WordFrequencyGame {
         }
     }
 
-    private List<WordInfo> generateWordFrequencyList(String sentence){
+    private List<WordInfo> generateWordFrequencyList(String sentence) {
         List<String> strings = Arrays.asList(sentence.split("\\s+"));
         List<WordInfo> list = new ArrayList<>();
-        for(String uniqueWord:new HashSet<>(strings)){
+        for (String uniqueWord : new HashSet<>(strings)) {
             long count = strings.stream().filter(word -> word.equals(uniqueWord)).count();
-            WordInfo wordInfo = new WordInfo(uniqueWord,(int)count);
+            WordInfo wordInfo = new WordInfo(uniqueWord, (int) count);
             list.add(wordInfo);
         }
         return list;
     }
+
     private String generateWordInfoFrequencyString(List<WordInfo> wordInfoList) {
         StringJoiner joiner = new StringJoiner("\n");
         for (WordInfo w : wordInfoList) {
-            String s = w.getValue() + " " +w.getWordCount();
+            String s = w.getValue() + " " + w.getWordCount();
             joiner.add(s);
         }
         return joiner.toString();
